fa031b25792deaa84f2299eeb5829f53eb6665f7
boradzhiev <lyubomir.boradzhiev@lidl.bg>
2021-05-21 12:57:37 +0300

    setting timer for search result click

--- core/shared/Navigate.ts
+++ core/shared/Navigate.ts
@@ -4,8 +4,10 @@ export const clickSelector = (selector: Selector) => {
     return t.click(selector)
 }
 
+//set wait timer becaouse there needs some time for search to load the results
 export const typeText = (selector: Selector, text: string) => {
     return t
     .typeText(selector, text)
+    .wait(100)
     .pressKey('Enter')
 }
